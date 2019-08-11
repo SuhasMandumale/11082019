09b0cc5bf3d75fddc696ed85344868326eddbc81
suhas mandumale <suhas.mandumale@gmail.com>
2019-08-11 19:34:12 +0530

    1st commit

---
+++ HelloWorlds/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HelloWorlds.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ HelloWorlds/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloWorlds.WebApiApplication" Language="C#" %>
